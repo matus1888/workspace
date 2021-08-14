dc9e494c96de67e13e7a449d557ce799c6f4c788
matus1888 <matus1888@gmail.com>
2021-08-14 19:22:10 +0300

    add date in visits

--- tst/queries/creaters.sql
+++ tst/queries/creaters.sql
@@ -1,13 +1,6 @@
 CREATE TABLE users (id  serial PRIMARY KEY,name varchar(255), password varchar(255) );
-CREATE TABLE articles (id serial PRIMARY KEY
-                        , date timestamptz
-                        , heading varchar(512)
-                        ,body text
-                        , userID INTEGER REFERENCES users (id));
-CREATE TABLE comments (id serial PRIMARY KEY
-                        ,userID INTEGER REFERENCES users (id)
-                        ,articleID INTEGER REFERENCES articles (id)
-                        ,date timestamptz, comment text);
+CREATE TABLE articles (id serial PRIMARY KEY, date timestamptz, heading varchar(512),body text, userID INTEGER REFERENCES users (id));
+CREATE TABLE comments (id serial PRIMARY KEY,userID INTEGER REFERENCES users (id),articleID INTEGER REFERENCES articles (id),date timestamptz, comment text);
 CREATE TABLE likes (id serial PRIMARY KEY
                         , articleID INTEGER REFERENCES articles (id)
                         , userID INTEGER REFERENCES users (id)
@@ -23,6 +16,7 @@ CREATE TABLE visits (id serial PRIMARY KEY
 ALTER TABLE articles ADD COLUMN body text;
 ALTER TABLE articles ADD COLUMN date timestamptz;
 ALTER TABLE users ADD COLUMN avatar text;
+ALTER TABLE visits ADD COLUMN date timetamptz;
 ALTER TABLE comments ADD COLUMN id serial PRIMARY KEY;
 ALTER TABLE likes ADD COLUMN id serial PRIMARY KEY;
 INSERT INTO comments (userID,articleID,date,comment) VALUES (1, 17, now(), 'первый комментарий');
